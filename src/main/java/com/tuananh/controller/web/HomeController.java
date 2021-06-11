46be97cc3ef55de152309232ee50ee79101005c1
unknown <tuananhtranha99@gmail.com>
2021-06-11 17:41:02 +0700

    Apply template bootstrap

--- src/main/java/com/tuananh/controller/web/HomeController.java
+++ src/main/java/com/tuananh/controller/web/HomeController.java
@@ -9,12 +9,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.tuananh.model.UserModel;
+
 @WebServlet(urlPatterns = {"/trang-chu"})
 public class HomeController extends HttpServlet{
 	private static final long serialVersionUID = 1L;
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		UserModel userModel = new UserModel();
+		userModel.setFullName("Hello World");
+		req.setAttribute("model", userModel);
 		RequestDispatcher rd = req.getRequestDispatcher("/views/web/home.jsp");
 		rd.forward(req, resp);
 	}
